00f5473ce06e44fe0d6b3132150a839691b1103a
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-07-11 13:56:11 +0100

    file button

---
+++ Smut/Scripts/Samples.js
@@ -0,0 +1,122 @@
+//Samples
+
+const var LoadSample1 = Content.getComponent("LoadSample1");
+const var LoadSample2 = Content.getComponent("LoadSample2");
+const var FileName1 = Content.getComponent("FileName1");
+const var FileName2 = Content.getComponent("FileName2");
+const var FilePath1 = Content.getComponent("FilePath1");
+const var FilePath2 = Content.getComponent("FilePath2");
+
+
+const var File1 = Synth.getAudioSampleProcessor("SMUT");
+
+const slot1 = File1.getAudioFile(0);
+const slot2 = File1.getAudioFile(1);
+
+reg lastPath1 = "";
+reg lastPath2 = "";
+reg isInternalSampleUpdate = false;
+
+const var defaultRef1 = "{PROJECT_FOLDER}AD-Draw1.wav";
+const var defaultRef = "{PROJECT_FOLDER}SINMIN.wav";
+
+// Delay ensures processors exist after compile
+Content.callAfterDelay(10, function()
+{
+    slot1.loadFile(defaultRef);
+	slot2.loadFile(defaultRef1);
+});
+
+inline function finishSampleLoad(slot, file, fileNameLabel, filePathLabel)
+{
+	isInternalSampleUpdate = true;
+
+	slot.loadFile(file.toString(0));
+	fileNameLabel.set("text", file.toString(1));
+	filePathLabel.set("text", file.toString(0));
+
+	Content.callAfterDelay(50, function()
+	{
+		SMUT.setBypassed(false);
+		isInternalSampleUpdate = false;
+	});
+}
+
+inline function recallSampleFromPath(slot, fileNameLabel, filePathLabel)
+{
+	local path = filePathLabel.get("text");
+
+	if (path == "")
+		return;
+
+	Engine.allNotesOff();
+	SMUT.setBypassed(true);
+
+	slot.loadFile(path);
+	fileNameLabel.set("text", FileSystem.fromAbsolutePath(path).toString(1));
+
+	Content.callAfterDelay(50, function()
+	{
+		SMUT.setBypassed(false);
+	});
+}
+
+inline function loadSampleForSlot(slot, fileNameLabel, filePathLabel)
+{
+	Engine.allNotesOff();
+	SMUT.setBypassed(true);
+
+	FileSystem.browse(FileSystem.AudioFiles, false, "*.wav;*.aif;*.aiff",
+		function [slot, fileNameLabel, filePathLabel](file)
+		{
+			finishSampleLoad(slot, file, fileNameLabel, filePathLabel);
+		}
+	);
+}
+
+inline function onLoadSample1Control(component, value)
+{
+	if (!value)
+		return;
+
+	loadSampleForSlot(slot1, FileName1, FilePath1);
+	component.setValue(0);
+}
+
+inline function onLoadSample2Control(component, value)
+{
+	if (!value)
+		return;
+
+	loadSampleForSlot(slot2, FileName2, FilePath2);
+	component.setValue(0);
+}
+
+LoadSample1.setControlCallback(onLoadSample1Control);
+LoadSample2.setControlCallback(onLoadSample2Control);
+
+Content.setSuspendTimerCallback(false);
+
+function onTimer()
+{
+	if (!isInternalSampleUpdate)
+	{
+		local newPath1 = FilePath1.get("text");
+		local newPath2 = FilePath2.get("text");
+
+		if (newPath1 != lastPath1)
+		{
+			lastPath1 = newPath1;
+			recallSampleFromPath(slot1, FileName1, FilePath1);
+		}
+
+		if (newPath2 != lastPath2)
+		{
+			lastPath2 = newPath2;
+			recallSampleFromPath(slot2, FileName2, FilePath2);
+		}
+	}
+}
+
+Synth.startTimer(200);
+
