475cfc9c6b520a4a0cd8de36f3fa5e35ffb6b1ce
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-07-13 14:47:55 +0100

    export again

--- Smut/CIExports/au/Source/PresetData.h
+++ Smut/CIExports/au/Source/PresetData.h
@@ -18,7 +18,7 @@ namespace PresetData
     const int           midiFilesSize = 28;
 
     extern const char*  preset;
-    const int           presetSize = 12875;
+    const int           presetSize = 12886;
 
     extern const char*  samplemaps;
     const int           samplemapsSize = 28;
